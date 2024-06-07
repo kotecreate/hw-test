3d7b542fe0d606bdbce8713f1acfacabb0658adc
kotecreate <kotecreate@gmail.com>
2024-06-07 11:04:37 +0300

    ver2

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -5,16 +5,26 @@ import "fmt"
 func main() {
 	var chessboard, one, two string
 	var a uint8
-	w, b := "  ##", "##  "
-	fmt.Printf("Введите размер доски: ")
-	fmt.Scanf("%v\n", &a)
-	sizeboard := int(a)
-	for i := 1; i <= sizeboard/2; i++ {
-		one += w
-		two += b
+	w, b := "  ", "##"
+	for {
+		fmt.Printf("Введите размер доски от 1 до 255: ")
+		fmt.Scanf("%v\n", &a)
+		if a > 0 && a <= 255 {
+			break
+		}
+		fmt.Println("Неверное значение")
 	}
-	for i := 1; i <= sizeboard/2; i++ {
-		chessboard = chessboard + one + "\n" + two + "\n"
+	sizeboard := int(a)
+	for i := 1; i <= sizeboard; i++ {
+		one += w + b
+		two += b + w
+	}
+	for i := 1; i <= sizeboard; i++ {
+		if i%2 != 0 {
+			chessboard += one + "\n"
+		} else {
+			chessboard += two + "\n"
+		}
 	}
 	fmt.Print(chessboard)
 }
