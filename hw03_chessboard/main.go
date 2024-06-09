975e9ae7f927f7257d9affb1825e8f5eef96d3d0
kotecreate <kotecreate@gmail.com>
2024-06-09 22:16:31 +0300

    fix 1.2

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -7,16 +7,21 @@ func main() {
 	var a uint8
 	var err error
 	w, b := "  ", "##"
-	fmt.Printf("Введите размер доски от 1 до 255: ")
+	fmt.Printf("Введите размер доски от 0 до 255: ")
 	_, err = fmt.Scanf("%v\n", &a)
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 		return
 	}
 	sizeboard := int(a)
-	for i := 1; i <= sizeboard/2; i++ {
-		one += w + b
-		two += b + w
+	for i := 1; i <= sizeboard; i++ {
+		if i%2 != 0 {
+			one += w
+			two += b
+		} else {
+			one += b
+			two += w
+		}
 	}
 	for i := 1; i <= sizeboard; i++ {
 		if i%2 != 0 {
