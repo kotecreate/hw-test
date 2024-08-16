4256e6c92e993ae32f5bb390e350d56691552e86
kotecreate <kotecreate@gmail.com>
2024-08-16 11:53:27 +0300

    Ver 1.3

--- hw08_binary_search/main.go
+++ hw08_binary_search/main.go
@@ -2,28 +2,14 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"sort"
-	"time"
 )
 
 func main() {
-	slice := randSli()
-	indVal := randVal(slice)
-	value := slice[indVal]
-	fmt.Printf("Создан массив: %v\n", slice)
+	slice := []int{5, 9, 14, 19, 21, 25, 35, 42}
+	value := slice[3]
+	fmt.Printf("Массив: %v\n", slice)
 	fmt.Printf("Найти: %v\n", value)
 	fmt.Printf("Искомое: %v\n", binaryS(slice, value))
-}
-
-func randSli() (res []int) { //создание рандомного слайса
-	randLen := rand.Intn(100)
-	for i := 0; i < randLen; i++ {
-		r := rand.New(rand.NewSource(time.Now().UnixNano()))
-		res = append(res, r.Int())
-	}
-	sort.Ints(res)
-	return res
 }
 
 func binaryS(sl []int, val int) int {
@@ -34,18 +20,14 @@ func binaryS(sl []int, val int) int {
 	for left <= right {
 		mid = (left + right) / 2
 		valMid = sl[mid]
-		if valMid == val {
+		switch left <= right {
+		case valMid == val:
 			return sl[mid]
-		} else if valMid < val {
+		case valMid < val:
 			left = mid + 1
-		} else {
+		default:
 			right = mid - 1
 		}
 	}
 	return valMid
-}
-
-func randVal(sl []int) int { //создание рандомного индекса в слайсе
-	v := rand.Intn(len(sl))
-	return v
 }
