52d0c2d45d9a50b0f0dccfa4d3b4f6b9682ed2c5
kotecreate <kotecreate@gmail.com>
2024-10-24 09:44:19 +0300

    Done

--- hw10_motion_sensor/main.go
+++ hw10_motion_sensor/main.go
@@ -1,5 +1,62 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
+func sensor(sens chan float64) {
+	data := 1.0
+	for {
+		select {
+		case sens <- data:
+			data += 1.0
+			time.Sleep(100 * time.Millisecond)
+		default:
+			fmt.Println("Channel is full, skipping data")
+			close(sens)
+			return
+		}
+	}
+}
+
+func processed(sens, res chan float64) {
+	timeout := time.After(1 * time.Minute)
+	for {
+		math := 0.0
+		select {
+		case <-timeout:
+			time.Sleep(1 * time.Second)
+			fmt.Println("Timeout")
+			close(res)
+			return
+		default:
+			for i := 0; i < 10; i++ {
+				val, ok := <-sens
+				if !ok {
+					fmt.Println("Channel is closed")
+					close(res)
+					return
+				}
+				math += val
+			}
+		}
+		res <- math / 10
+	}
+}
+
 func main() {
-	// Place your code here.
+	sens := make(chan float64)
+	res := make(chan float64)
+
+	go sensor(sens)
+	go processed(sens, res)
+	for {
+		val, ok := <-res
+		if !ok {
+			fmt.Println("Канал передачи данных закрыт")
+			return
+		}
+		fmt.Println("Среднее арифмитическое: ", val)
+	}
 }
---
+++ hw10_motion_sensor/monitor_sensor_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestSensor(t *testing.T) {
+	testSens := make(chan float64)
+	go sensor(testSens)
+
+}
+
+func TestProcessed(t *testing.T) {
+	testSens := make(chan float64)
+	testResult := make(chan float64)
+	go sensor(testSens)
+	go processed(testSens, testResult)
+
+}
