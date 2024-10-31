22f0c862e58c353056d618e5315e22eadc3febb6
kotecreate <kotecreate@gmail.com>
2024-10-31 23:55:14 +0300

    Done

--- hw11_worker_pool/main.go
+++ hw11_worker_pool/main.go
@@ -1,5 +1,46 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
+var count = 0
+
+func r1(m *sync.Mutex, wg *sync.WaitGroup) {
+	defer wg.Done()
+	m.Lock()
+	count++
+	fmt.Printf("Счетчик 1: %d\n", count)
+	m.Unlock()
+}
+
+func r2(m *sync.Mutex, wg *sync.WaitGroup) {
+	defer wg.Done()
+	m.Lock()
+	count++
+	fmt.Printf("Счетчик 2: %d\n", count)
+	m.Unlock()
+}
+
+func r3(m *sync.Mutex, wg *sync.WaitGroup) {
+	defer wg.Done()
+	m.Lock()
+	count++
+	fmt.Printf("Счетчик 3: %d\n", count)
+	m.Unlock()
+}
+
 func main() {
-	// Place your code here.
+	var mutex sync.Mutex
+	var wg sync.WaitGroup
+
+	for i := 0; i < 500; i++ {
+		wg.Add(3)
+		go r1(&mutex, &wg)
+		go r2(&mutex, &wg)
+		go r3(&mutex, &wg)
+	}
+	wg.Wait()
+	fmt.Println("Итог: ", count)
 }
